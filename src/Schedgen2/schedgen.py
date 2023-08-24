b99cceff550e3d1c7949105cd0c7825c63adfd17
Nils Blach <nils.blach@gmx.de>
2023-08-24 13:31:00 +0200

    Add Dissemination to patterns + Fix bug where fh was interpreted as labeller

--- src/Schedgen2/schedgen.py
+++ src/Schedgen2/schedgen.py
@@ -4,7 +4,7 @@ from goal import GoalComm
 from patterns import binomialtree, dissemination
 
 parser = argparse.ArgumentParser(description="Generate GOAL Schedules.")
-parser.add_argument("--ptrn", dest="ptrn", choices=["binomialtreereduce", "binarytreebcast"])
+parser.add_argument("--ptrn", dest="ptrn", choices=["binomialtreereduce", "binarytreebcast", "dissemination"], help="Pattern to generate", required=True)
 parser.add_argument("--commsize", dest="commsize", type=int, default=8, help="Size of the communicator")
 parser.add_argument("--datasize", dest="datasize", type=int, default=8, help="Size of the data, i.e., for reduce operations")
 
@@ -14,12 +14,12 @@ args = parser.parse_args()
 
 if args.ptrn == "binomialtreereduce":
     g = binomialtree(args.commsize, args.datasize, 42, "reduce")
-    g.write_goal(sys.stdout)
+    g.write_goal(fh=sys.stdout)
 
 if args.ptrn == "binomialtreebcast":
     g = binomialtree(args.commsize, args.datasize, 42, "bcast")
-    g.write_goal(sys.stdout)
+    g.write_goal(fh=sys.stdout)
 
 if args.ptrn == "dissemination":
     g = dissemination(args.commsize, args.datasize, 42)
-    g.write_goal(sys.stdout)
+    g.write_goal(fh=sys.stdout)
