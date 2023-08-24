7c5c4556d2dc4a39741753a1961d154f3edcdd15
Nils Blach <nils.blach@gmx.de>
2023-08-24 14:25:32 +0200

    Adding ring-allreduce + Adding writing to output file capabilites + formatting

--- src/Schedgen2/goal.py
+++ src/Schedgen2/goal.py
@@ -1,7 +1,7 @@
 import sys
 
-class GoalLabeller:
 
+class GoalLabeller:
     def __init__(self):
         self.next_label = 1
         self.next_comm = 1
@@ -25,11 +25,11 @@ class GoalLabeller:
         return self.comm_dict[comm]
 
     def MakeTag(self, tag, comm):
-        """ Combine the user tag and the comm tag portion """
+        """Combine the user tag and the comm tag portion"""
         return tag * 1000 + comm
 
-class GoalOp:
 
+class GoalOp:
     def __init__(self):
         self.depends_on = []
 
@@ -39,7 +39,6 @@ class GoalOp:
 
 
 class GoalSend(GoalOp):
-
     def __init__(self, dst, tag, size):
         super().__init__()
         self.dst = dst
@@ -47,13 +46,17 @@ class GoalSend(GoalOp):
         self.size = size
 
     def write_goal(self, labeller, fh, comm, basecomm):
-        fh.write("l{label}: send {size}b to {dst} tag {tag}\n".format(label=labeller.GetLabel(self), 
-                                                                     size=str(self.size), 
-                                                                     dst=str(comm.TranslateRank(self.dst, basecomm)),
-                                                                     tag=str(labeller.MakeTag(self.tag, labeller.GetCommID(comm)))))
+        fh.write(
+            "l{label}: send {size}b to {dst} tag {tag}\n".format(
+                label=labeller.GetLabel(self),
+                size=str(self.size),
+                dst=str(comm.TranslateRank(self.dst, basecomm)),
+                tag=str(labeller.MakeTag(self.tag, labeller.GetCommID(comm))),
+            )
+        )
+
 
 class GoalRecv(GoalOp):
-
     def __init__(self, src, tag, size):
         super().__init__()
         self.src = src
@@ -61,24 +64,30 @@ class GoalRecv(GoalOp):
         self.size = size
 
     def write_goal(self, labeller, fh, comm, basecomm):
-        fh.write("l{label}: recv {size}b from {src} tag {tag}\n".format(label=labeller.GetLabel(self), 
-                                                                       size=str(self.size), 
-                                                                       src=str(comm.TranslateRank(self.src, basecomm)),
-                                                                       tag=str(labeller.MakeTag(self.tag, labeller.GetCommID(comm)))))
+        fh.write(
+            "l{label}: recv {size}b from {src} tag {tag}\n".format(
+                label=labeller.GetLabel(self),
+                size=str(self.size),
+                src=str(comm.TranslateRank(self.src, basecomm)),
+                tag=str(labeller.MakeTag(self.tag, labeller.GetCommID(comm))),
+            )
+        )
+
 
 class GoalCalc(GoalOp):
-
     def __init__(self, size):
         super().__init__()
         self.size = size
 
     def write_goal(self, labeller, fh, comm, basecomm):
-        fh.write("l{label}: calc {size}\n".format(label=labeller.GetLabel(self), size=str(self.size)))
-
+        fh.write(
+            "l{label}: calc {size}\n".format(
+                label=labeller.GetLabel(self), size=str(self.size)
+            )
+        )
 
 
 class GoalRank:
-
     def __init__(self, comm, rank):
         self.comm = comm
         self.rank = rank
@@ -87,14 +96,14 @@ class GoalRank:
 
     def Send(self, dst, tag, size):
         if dst > self.comm.CommSize():
-            raise ValueError(str(dst)+" is larger than comm size!")
+            raise ValueError(str(dst) + " is larger than comm size!")
         op = GoalSend(dst=dst, tag=tag, size=size)
         self.ops.append(op)
         return op
 
     def Recv(self, src, tag, size):
         if src > self.comm.CommSize():
-            raise ValueError(str(src)+" is larger than comm size!")
+            raise ValueError(str(src) + " is larger than comm size!")
         op = GoalRecv(src=src, tag=tag, size=size)
         self.ops.append(op)
         return op
@@ -104,18 +113,22 @@ class GoalRank:
         self.ops.append(op)
         return op
 
-
-
     def write_goal(self, labeller, fh, rankid=True, basecomm=None):
         if basecomm is None:
-            basecomm = self.comm # stupid python evals default args at method definition, not call time :(
+            basecomm = (
+                self.comm
+            )  # stupid python evals default args at method definition, not call time :(
         if rankid:
-            fh.write("rank "+str(self.rank)+" {\n")
+            fh.write("rank " + str(self.rank) + " {\n")
         for op in self.ops:
             op.write_goal(labeller, fh, self.comm, basecomm)
         for op in self.ops:
             for req in op.depends_on:
-                fh.write("l{label1} requires l{label2}\n".format(label1=labeller.GetLabel(op), label2=labeller.GetLabel(req)))
+                fh.write(
+                    "l{label1} requires l{label2}\n".format(
+                        label1=labeller.GetLabel(op), label2=labeller.GetLabel(req)
+                    )
+                )
         for sc in self.comm.subcomms:
             sc.write_goal_subcomm(labeller, fh, self.rank, basecomm)
         if rankid:
@@ -123,7 +136,6 @@ class GoalRank:
 
 
 class GoalComm:
-
     def __init__(self, comm_size):
         self.base_comm = self
         self.comm_size = comm_size
@@ -147,50 +159,59 @@ class GoalComm:
 
     def CommSplit(self, color, key):
         if len(list(color)) < self.comm_size or len(list(key)) < self.comm_size:
-            raise ValueError("The length of color and key array must match the communicator size.")
+            raise ValueError(
+                "The length of color and key array must match the communicator size."
+            )
         newcomms = []
-        order = [ (oldrank, color[oldrank], key[oldrank]) for oldrank in range(0, self.comm_size) ]
+        order = [
+            (oldrank, color[oldrank], key[oldrank])
+            for oldrank in range(0, self.comm_size)
+        ]
         color_buckets = {}
         for o in order:
             if o[1] in color_buckets:
                 color_buckets[o[1]].append(o)
             else:
-                color_buckets[o[1]]=[o]
+                color_buckets[o[1]] = [o]
         for c in color_buckets.keys():
-            c_list = sorted(color_buckets[c], key=lambda x: x[2]) # sort by key within color
+            c_list = sorted(
+                color_buckets[c], key=lambda x: x[2]
+            )  # sort by key within color
             nc = GoalComm(len(c_list))
             nc.base_comm = self
             for idx, r in enumerate(nc):
-                r.base_rank = c_list[idx][0] # store the rank the new rank had in the comm it was splitted from
+                r.base_rank = c_list[idx][
+                    0
+                ]  # store the rank the new rank had in the comm it was splitted from
             newcomms.append(nc)
         self.subcomms += newcomms
         return newcomms
 
     def write_goal(self, labeller=None, fh=sys.stdout):
-        fh.write("num_ranks "+str(len(self.ranks))+"\n\n")
+        fh.write("num_ranks " + str(len(self.ranks)) + "\n\n")
         if labeller is None:
             labeller = GoalLabeller()
         for r in self.ranks:
             r.write_goal(labeller, fh, rankid=True, basecomm=self)
 
     def write_goal_subcomm(self, labeller, fh, rank, basecomm):
-        """ if this comm has a rank with base_rank=rank, print its goal ops without enclosing brackets """
+        """if this comm has a rank with base_rank=rank, print its goal ops without enclosing brackets"""
         for r in self.ranks:
             if r.base_rank == rank:
                 r.write_goal(labeller, fh, rankid=False, basecomm=basecomm)
 
     def TranslateRank(self, rank, basecomm):
-        """ Find out the rank id of the given rank (in self) in basecomm """
+        """Find out the rank id of the given rank (in self) in basecomm"""
         if self == basecomm:
             return rank
         if rank == None:
             raise ValueError("Attempt to translate a non-existing rank!")
         return self.base_comm.TranslateRank(self.ranks[rank].base_rank, basecomm)
 
+
 if __name__ == "__main__":
-    
     comm_world = GoalComm(4)
-    comms = comm_world.CommSplit(color=[0,0,1,1], key=[0,1,2,3])
+    comms = comm_world.CommSplit(color=[0, 0, 1, 1], key=[0, 1, 2, 3])
     comms[0][0].Send(1, 42, 32)
     comms[0][1].Recv(0, 42, 32)
     comms[1][1].Send(0, 42, 16)
@@ -199,4 +220,3 @@ if __name__ == "__main__":
     comms[0].write_goal()
     comms[1].write_goal()
     comm_world.write_goal()
-
--- src/Schedgen2/patterns.py
+++ src/Schedgen2/patterns.py
@@ -1,29 +1,32 @@
 from math import ceil, log2
 from goal import GoalComm
 
+
 def binomialtree(comm_size, datasize, tag, dir="reduce"):
     comm = GoalComm(comm_size)
     for rank in range(0, comm_size):
         send = None
         recv = None
         for r in range(0, ceil(log2(comm_size))):
-            peer = rank + pow(2, r);
-            if ((rank + pow(2, r) < comm_size) and (rank < pow(2, r))):
+            peer = rank + pow(2, r)
+            if (rank + pow(2, r) < comm_size) and (rank < pow(2, r)):
                 if dir == "reduce":
-                    recv = comm.Recv(size=datasize, src=peer, dst=rank, tag=tag);
+                    recv = comm.Recv(size=datasize, src=peer, dst=rank, tag=tag)
                 elif dir == "bcast":
-                    send = comm.Send(size=datasize, dst=peer, src=rank, tag=tag);
+                    send = comm.Send(size=datasize, dst=peer, src=rank, tag=tag)
                 else:
-                    raise ValueError("direction "+str(dir)+" in binomialtree not implemented.")
-            if ((send is not None) and (recv is not None)):
-                send.requires(recv);
-            peer = rank - pow(2, r);
-            if ((rank >= pow(2, r)) and (rank < pow(2, r + 1))):
+                    raise ValueError(
+                        "direction " + str(dir) + " in binomialtree not implemented."
+                    )
+            if (send is not None) and (recv is not None):
+                send.requires(recv)
+            peer = rank - pow(2, r)
+            if (rank >= pow(2, r)) and (rank < pow(2, r + 1)):
                 if dir == "reduce":
-                    send = comm.Send(size=datasize, dst=peer, src=rank, tag=tag);
+                    send = comm.Send(size=datasize, dst=peer, src=rank, tag=tag)
                 if dir == "bcast":
-                    recv = comm.Recv(size=datasize, src=peer, dst=rank, tag=tag);
-                    
+                    recv = comm.Recv(size=datasize, src=peer, dst=rank, tag=tag)
+
     return comm
 
 
@@ -32,10 +35,46 @@ def dissemination(comm_size, datasize, tag):
     for rank in range(0, comm_size):
         dist = 1
         recv = None
-        while (dist < comm_size):
-            send = comm.Send(src=rank, dst=(rank+dist+comm_size)%comm_size, size=datasize, tag=tag)
+        while dist < comm_size:
+            send = comm.Send(
+                src=rank,
+                dst=(rank + dist + comm_size) % comm_size,
+                size=datasize,
+                tag=tag,
+            )
             if recv is not None:
                 send.requires(recv)
-            recv = comm.Recv(src=(rank-dist+comm_size)%comm_size, dst=rank, size=datasize, tag=tag)
+            recv = comm.Recv(
+                src=(rank - dist + comm_size) % comm_size,
+                dst=rank,
+                size=datasize,
+                tag=tag,
+            )
             dist *= 2
-    return comm	
+    return comm
+
+
+def ring_allreduce(comm_size, datasize, base_tag):
+    comm = GoalComm(comm_size)
+    for rank in range(0, comm_size):
+        recv = None
+        send = None
+        chunk_size = (
+            datasize // comm_size
+            if datasize % comm_size == 0
+            else datasize // comm_size + 1
+        )
+        for _ in range(2):
+            # Phase 0: reduce-scatter, Phase 1: allgather
+            for _ in range(0, comm_size - 1):
+                send = comm.Send(
+                    src=rank, dst=(rank + 1) % comm_size, size=chunk_size, tag=base_tag
+                )
+                if recv is not None:
+                    send.requires(recv)
+                recv = comm.Recv(
+                    src=(rank - 1) % comm_size, dst=rank, size=chunk_size, tag=base_tag
+                )
+            # update tag for next phase
+            base_tag += 1
+    return comm
